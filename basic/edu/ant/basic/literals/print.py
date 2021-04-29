969072fc4280a9787067ae900adccd93463cb61c
mzacki <zacki.mateusz@gmail.com>
2021-04-29 20:35:45 +0200

    print strings

---
+++ basic/edu/ant/basic/literals/print.py
@@ -0,0 +1,18 @@
+print(2 * 5)
+print()  # prints empty line
+print("Prints multiple", "args", "even ints like", 4)  # accepts numeric literals, prints like strings
+print('Single or double quotes')
+print("  ' '  ")  # quotes within quotes
+
+# string vars declaration
+string = "typische string"
+string2 = "another string"
+print(string + ' ' + string2)
+
+# stdin input example with prompt
+text = input("Enter something and press enter....")
+print("Entered: " + text)
+
+# stdin without prompt
+text2 = input()
+print("Without prompt: " + text2)
