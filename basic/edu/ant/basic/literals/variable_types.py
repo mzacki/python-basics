8be3fb286881eed1fefc6b8175efdf15c840c04f
mzacki <zacki.mateusz@gmail.com>
2021-05-05 19:50:58 +0200

    dynamically typed

---
+++ basic/edu/ant/basic/literals/variable_types.py
@@ -0,0 +1,10 @@
+
+# dynamically typed
+# changing var type is not possible in Java
+sth = 23
+print("Initial type: ", type(sth))
+sth = "23"
+print("Current type: ", type(sth))
+
+
+# strongly typed
