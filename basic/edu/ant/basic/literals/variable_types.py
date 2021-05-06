758e1b66b90257cd3594a2aa11c1f1f76a423978
mzacki <zacki.mateusz@gmail.com>
2021-05-06 19:02:10 +0200

    strongly typed

--- basic/edu/ant/basic/literals/variable_types.py
+++ basic/edu/ant/basic/literals/variable_types.py
@@ -1,10 +1,11 @@
-
 # dynamically typed
 # changing var type is not possible in Java
 sth = 23
-print("Initial type: ", type(sth))
+print("Initial type: ", type(sth))  # int
 sth = "23"
-print("Current type: ", type(sth))
-
+print("Current type: ", type(sth))  # string
 
 # strongly typed
+# e.g. no automatic type conversion
+# explicit conversion is always needed
+# print(sth + 23) ---> type error
