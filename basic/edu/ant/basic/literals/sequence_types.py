b50e78f70d7cc110f4b039a0f56ee29523a84dd3
mzacki <zacki.mateusz@gmail.com>
2021-05-11 17:54:00 +0200

    sequence types

---
+++ basic/edu/ant/basic/literals/sequence_types.py
@@ -0,0 +1,21 @@
+# sequence types in python3
+# string
+hobby = "beekeeping"
+# list
+apiary = ["bees", "queen", "beehive", "beekeeper", "flower"]
+# tuple
+# range
+# byte, byte array
+
+# accessing like 2D Java arrays or list of lists
+print(apiary[0][0])
+# printing x times
+print(apiary * 5)
+print("string" * 5)
+# concat list like string
+print(apiary + apiary)
+# contains (list)
+print("bees" in apiary)
+print("cat" in apiary)
+# contains (string)
+print("bee" in hobby)
