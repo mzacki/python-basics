b86fd8af7e9c5330030e4e61715c1a7d9320539a
mzacki <zacki.mateusz@gmail.com>
2021-05-12 18:30:02 +0200

    string formatting

--- basic/edu/ant/basic/literals/string_utils.py
+++ basic/edu/ant/basic/literals/string_utils.py
@@ -24,3 +24,11 @@ print(title[-21:8])
 # step slicing
 print(title[:21:2])  # every second char
 print(title[0:21:5])  # every fifth char
+
+# conversion to string: replacement fields (instead of str())
+number = 10
+print("The number equals {0}".format(number))
+print("There are {0} empty slots: {1}, {2}".format(2, "one", "two"))
+
+# f-string
+print(f"An f-string formatter of {number} number")
