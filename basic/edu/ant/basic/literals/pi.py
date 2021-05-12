34569d4203996fa0702fa6f9e07fe05328af492e
mzacki <zacki.mateusz@gmail.com>
2021-05-12 18:30:13 +0200

    pi

---
+++ basic/edu/ant/basic/literals/pi.py
@@ -0,0 +1,6 @@
+# floating point formatting
+print("PI equals {0}".format(22 / 7))
+# 2 decimal places
+print("PI equals {0:.2f}".format(22 / 7))
+# 10 decimal places
+print("PI equals {0:.10f}".format(22 / 7))
