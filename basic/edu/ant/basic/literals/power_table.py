b165ebc431cc6830fd265d4e287e8dea12cb523a
mzacki <zacki.mateusz@gmail.com>
2021-05-12 18:30:23 +0200

    power

---
+++ basic/edu/ant/basic/literals/power_table.py
@@ -0,0 +1,11 @@
+# power
+for i in range(0, 11):
+    print(
+        """{0} \t \tto the power of 0 is {1}, 
+        to the power of 1 is {2}, 
+        to the power of 2 (squared) is {3}, 
+        to the power of 3 (cubed) is {4}""".format(
+            i, i ** 0, i ** 1, i ** 2, i ** 3))
+
+# more formatting options:
+# {0:2}, {0:<3}, etc.
