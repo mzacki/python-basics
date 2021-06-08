e0e5ab00b40c284c4d3c81882414ca21efdf30de
mzacki <zacki.mateusz@gmail.com>
2021-06-08 22:07:15 +0200

    api tester

---
+++ tools/edu/ant/tools/tester/api_tester.py
@@ -0,0 +1,24 @@
+import json
+import requests
+from requests import codes
+from edu.ant.tools.tester.properties import get_properties
+
+# test case for sample api
+# api call
+url = get_properties().get("api_1").get("host")
+key = get_properties().get("api_1").get("key")
+response = requests.get(url + key)
+
+# print for dev only
+print(response)
+response_body = (json.dumps(response.json()))
+# print for dev only
+print(response_body)
+
+# given
+expected_value = json.loads(response_body)
+timestep = (expected_value["data"]["timelines"][0]["timestep"])
+
+# assert
+assert response.status_code == codes.ok
+assert timestep == "1h"
