bb117ce41e7d86cc9b41de1d9a6d1ae3cc2e8437
mzacki <38253922+mzacki@users.noreply.github.com>
2021-09-01 19:25:19 +0200

    ohm's law

--- basic/edu/ant/basic/math/parallel.py
+++ basic/edu/ant/basic/math/parallel.py
@@ -1,5 +1,15 @@
 from sympy import simplify, symbols, pprint
 
+# Ohm's law:
+# I = V / R
+# or V = RI
+# where:
+# I -> current
+# V -> voltage
+# R -> resistance
+
+
+
 r, r1, r2, v, i1, i2 = symbols("r r1 r2 v i1 i2")
 i1 = v / r1
 i2 = v / r2
