59712c13774e5ece009a5bf2d3beb8f64872ad87
mzacki <38253922+mzacki@users.noreply.github.com>
2021-10-20 17:32:02 +0200

    zip

---
+++ basic/edu/ant/basic/useful/zip.py
@@ -0,0 +1,6 @@
+# creates tuples containing items of the same index
+List_1 = [1, 2, 3, 4, 5]
+List_2 = [6, 7, 8, 9, 10]
+List_3 = [11, 12, 13, 14, 15]
+zipped = zip(List_1, List_2, List_3)
+print(zipped)
