c4f1d3e5777afebc23835e6e86b8fb1de436b009
mzacki <38253922+mzacki@users.noreply.github.com>
2021-10-20 17:22:05 +0200

    enumerate

---
+++ basic/edu/ant/basic/useful/enumerate.py
@@ -0,0 +1,5 @@
+# Enumerate returns both an iterator and a value from a data set.
+# No need to increase counter
+Data = [18, 24, 5, 17, 21, 19]
+for i, value in enumerate(Data):
+    print(i, value)
