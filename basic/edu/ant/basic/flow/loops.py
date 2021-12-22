83a5707fe3767a3fff8601709f6849b8311921d1
mzacki <38253922+mzacki@users.noreply.github.com>
2021-12-22 19:21:23 +0100

    for loop

--- basic/edu/ant/basic/flow/loops.py
+++ basic/edu/ant/basic/flow/loops.py
@@ -5,6 +5,10 @@ for char in string:
 
 # first argument inclusive
 # last argument exclusive
+# no start index defaults to 0
 for i in range(1, 20):
     print(i)
 
+# counts to zero step -1
+for i in range(10, -1, -1):
+    print(i)
