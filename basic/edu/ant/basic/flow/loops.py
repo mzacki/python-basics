2ca6bbdb5e79f1e19601878c03e8cf9d3f180813
mzacki <38253922+mzacki@users.noreply.github.com>
2022-01-04 20:52:46 +0100

    nested for loop

--- basic/edu/ant/basic/flow/loops.py
+++ basic/edu/ant/basic/flow/loops.py
@@ -12,3 +12,10 @@ for i in range(1, 20):
 # counts to zero step -1
 for i in range(10, -1, -1):
     print(i)
+
+# nested for loops
+print('Chess coordinates:')
+for letter in range(ord('a'), ord('h') + 1):
+    for number in range(1, 9):
+        # end parameter prints without new line
+        print(chr(letter) + str(number), end=' ')
