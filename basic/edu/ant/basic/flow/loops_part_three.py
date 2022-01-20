a462cf28026091c75232326b476ccb1a1efc8768
mzacki <38253922+mzacki@users.noreply.github.com>
2022-01-20 17:17:28 +0100

    while loop

--- basic/edu/ant/basic/flow/loops_part_three.py
+++ basic/edu/ant/basic/flow/loops_part_three.py
@@ -1,4 +1,10 @@
 # while loop
+
+# for i in range(10):
+# print("Current value of i is {}".format(i))
+# same result, but contrary to compiled languanges beneath it's different code
+# in C or Java it would compile into the same bytecode
+
 i = 0
 while i < 10:
     print("Current value of i is {}".format(i))
