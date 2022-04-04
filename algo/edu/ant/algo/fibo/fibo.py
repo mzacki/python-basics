173acd4a77523798681a4733b54ee27190e68b9c
mzacki <38253922+mzacki@users.noreply.github.com>
2022-04-04 13:49:51 +0200

    fibonacci

---
+++ algo/edu/ant/algo/fibo/fibo.py
@@ -0,0 +1,17 @@
+def create_fibo_series(last_n):
+    sequence = [0] * last_n
+
+    sequence[0] = 0
+
+    if last_n > 1:
+        sequence[1] = 1
+
+    for i in range(2, last_n):
+        sequence[i] = sequence[i - 2] + sequence[i - 1]
+
+    return sequence
+
+
+def test_create_fibo_series():
+
+    assert create_fibo_series(3) == [0, 1, 1]
