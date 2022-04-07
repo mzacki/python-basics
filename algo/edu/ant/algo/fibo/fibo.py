d38aadb2bd827a5cc8f5376fc6f37ee7909bbfdb
mzacki <38253922+mzacki@users.noreply.github.com>
2022-04-07 11:27:35 +0200

    fibonacci

--- algo/edu/ant/algo/fibo/fibo.py
+++ algo/edu/ant/algo/fibo/fibo.py
@@ -12,8 +12,29 @@ def create_fibo_series(last_n):
     return sequence
 
 
-def test_create_fibo_series():
+def fibo_faster(n):
+    current = 0
+    one_fib_ago = 1
+    two_fib_ago = 0
 
+    for i in range(2, n + 1):
+        current = two_fib_ago + one_fib_ago
+        if i == 2:
+            continue
+        two_fib_ago = one_fib_ago
+        one_fib_ago = current
+
+    return current
+
+
+def test_create_fibo_series():
     assert create_fibo_series(3) == [0, 1, 1]
     assert create_fibo_series(1) == [0]
     assert create_fibo_series(2) == [0, 1]
+
+
+def test_fibo_faster():
+    assert fibo_faster(1) == 0
+    assert fibo_faster(2) == 1
+    assert fibo_faster(3) == 1
+    assert fibo_faster(4) == 2
