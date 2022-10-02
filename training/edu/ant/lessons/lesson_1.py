d096336e8031550821aaeb65247b82812bf5da44
mzacki <38253922+mzacki@users.noreply.github.com>
2022-10-02 18:28:16 +0200

    training

---
+++ training/edu/ant/lessons/lesson_1.py
@@ -0,0 +1,27 @@
+# function to reverse string starting from last index (-1)
+def reverse(s):
+    return s[::-1]
+
+
+# assignment to a variable (cokolwiek), value is "cokolwiek"
+cokolwiek = "cokolwiek"
+# tabela / array - example:
+# [1, 2, 3, 4]
+
+# get value (character) by index (here: 7)
+# dowolna_litera = cokolwiek[7]
+
+# print result of function cokolwiek
+print(cokolwiek[-2])
+
+# print(reverse(cokolwiek))
+
+# anything = "Anything"
+
+# print(anything)
+
+# TODO potęgowanie w Pythonie (power)
+# TODO pierwiastkowanie (square root, cube root)
+# TODO pobieranie bieżącej daty, czasu (how to get current date in Python)
+
+# see more materials: edu/ant/basic/literals/string_utils.py
